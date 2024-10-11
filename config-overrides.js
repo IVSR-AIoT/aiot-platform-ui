d9ac98ddae1beab240926ccf5d2a0d213d1b049e
Cong Minh <congndm@rabiloo.com>
2024-10-11 08:57:40 +0700

    feat/config-webpack-to-other-device-connect

--- config-overrides.js
+++ config-overrides.js
@@ -1,6 +1,18 @@
 const { override, useBabelRc } = require('customize-cra');
 
-module.exports = override(
-    // eslint-disable-next-line react-hooks/rules-of-hooks
-    useBabelRc(),
-);
+module.exports = {
+    webpack: override(
+        // eslint-disable-next-line react-hooks/rules-of-hooks
+        useBabelRc(),
+    ),
+    devServer: (configFunction) => {
+        return (proxy, allowedHost) => {
+            const config = configFunction(proxy, allowedHost);
+
+            config.port = 1911;
+            config.allowedHosts = 'all';
+
+            return config;
+        };
+    },
+};
