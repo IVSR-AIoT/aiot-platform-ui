52ae079ae50fc0d18a6f9631b39d2197199c627e
Khai Nguyen <104447228+Khainq1911@users.noreply.github.com>
2024-11-08 17:18:59 +0700

    feat: device (#28)
    * feat: manage projects
    
    * feat: add and delete users
    
    * feat: add confirm modal for user deletion
    
    * feat: device screen
    
    * complete device page
    
    * websocket in progress
    
    * fix: websocket context
    
    * fix: change title icon
    
    * fix
    
    ---------
    
    Co-authored-by: Cong Minh <ndmc.system176@gmail.com>

---
+++ src/components/updateDeviceModal.js
@@ -0,0 +1,83 @@
+import { Form, Input, message, Modal, Select } from 'antd';
+import { getListProjectService } from '~/services/projectServices';
+import { useEffect, useState } from 'react';
+import { updateDeviceService } from '~/services/deviceService';
+import { isUser } from '~/hook/useAuth';
+
+export default function UpdateDeviceModal({ openModal, setOpenModal, device, getListDevices, projectIdInDevice }) {
+    const [form] = Form.useForm();
+    const [selectOptions, setSelectOptions] = useState([]);
+    const listProjects = async () => {
+        if (isUser()) {
+            return;
+        }
+        try {
+            const res = await getListProjectService();
+            setSelectOptions(res.map((project) => ({ label: project.name, value: project.id })));
+        } catch (error) {
+            console.log(error);
+            message.error('Failed to load project list. Please try again later.');
+        }
+    };
+
+    useEffect(() => {
+        listProjects();
+    }, []);
+
+    const onFinish = async (value) => {
+        value.projectId = value.projectId ? value.projectId : null;
+        try {
+            await updateDeviceService(device?.id, value);
+            message.success('Device updated successfully!');
+            getListDevices();
+        } catch {
+            message.error('Failed to update the device. Please try again.');
+        }
+
+        form.resetFields();
+    };
+
+    useEffect(() => {
+        if (device) {
+            const projectId = selectOptions?.find((item) => device.projectId === item.value);
+            form.setFieldsValue({ name: device.name || '', projectId: projectId });
+        }
+    });
+
+    const handleOk = () => {
+        form.submit();
+        setOpenModal(false);
+    };
+
+    const handleCancel = () => {
+        setOpenModal(false);
+        form.resetFields();
+    };
+
+    return (
+        <Modal
+            title={<h1 className="font-semibold text-[20px]">Update Project</h1>}
+            open={openModal}
+            onOk={handleOk}
+            onCancel={handleCancel}
+        >
+            <Form
+                form={form}
+                onFinish={onFinish}
+                labelCol={{
+                    span: 5,
+                }}
+                wrapperCol={{
+                    span: 18,
+                }}
+            >
+                <Form.Item label={<h1 className="font-medium">Device Name</h1>} name={'name'}>
+                    <Input />
+                </Form.Item>
+                <Form.Item label={<h1 className="font-medium">Select Project</h1>} name={'projectId'}>
+                    <Select options={selectOptions} allowClear />
+                </Form.Item>
+            </Form>
+        </Modal>
+    );
+}
---
+++ src/services/deviceService.js
@@ -0,0 +1,16 @@
+import apiInstances from './api';
+
+export const deviceListService = async () => {
+    const response = await apiInstances.get('/device');
+    return response;
+};
+
+export const listDeviceByProjectIdService = async (id) => {
+    const response = await apiInstances.get(`/device?projectId=${id}`);
+    return response;
+};
+
+export const updateDeviceService = async (id, value) => {
+    const response = await apiInstances.put(`/device/${id}`, value);
+    return response;
+};
